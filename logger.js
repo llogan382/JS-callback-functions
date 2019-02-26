a248f1e7a0c6037a51fe4985a3a2fca65c238e6f
Luke Logan <llogan382@gmail.com>
2019-02-26 06:26:00 -0500

    practicing and drilling callback functions. For some reason these are hard for me

--- logger.js
+++ logger.js
@@ -3,8 +3,9 @@ const person = {
     role: 'JavaScript Teacher'
 }
 
+// Function name is "logateacher" and it takes what is in the parens
 function logTeacher(teacher) {
     console.log(`${teacher.name} - ${teacher.role}`);
 }
-
+// Person object is passed into the "logTeacher" function, and is named "teacher" within the function scope (above)
 logTeacher(person);
