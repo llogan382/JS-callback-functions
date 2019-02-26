ade6fac3516036e987e7a8022e9a96b45ef1c46b
Luke Logan <llogan382@gmail.com>
2019-02-26 06:42:00 -0500

    adds notes to explain the callback

--- callback.js
+++ callback.js
@@ -1,9 +1,18 @@
-function sayHello() {
-    console.log('Hello');
-}
+// function sayHello(){
+//     console.log('hello');
+// }
+
 
 function executeCallback( callback ){
     callback();
 };
 
-executeCallback(sayHello);
+// Instead of having the function listed above,
+// You can pass in the function as an anonymous function, like this:
+executeCallback( function () {
+    console.log('Hello');
+});
+
+executeCallback( function () {
+    console.log('GoodBye');
+});
