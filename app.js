0543061ed0dc04fd18eb6f07e8a7e3dda65666a5
Luke Logan <llogan382@gmail.com>
2019-02-28 06:52:15 -0500

    adds files for targeting an element on focus

--- app.js
+++ app.js
@@ -1,26 +1,4 @@
- 
-const div1 = document.getElementById('first');
-const div2 = document.getElementById('second');
-const div3 = document.getElementById('third');
+const nameInput = document.getElementById('name');
+const messageTextArea = document.getElementById('message');
 
-function makeRed(element) {
-    element.style.backgroundColor = "red";
-}
 
-function makeWhite(element) {
-    element.style.backgroundColor = "white";
-}
-
-function makeBlue(element) {
-    element.style.backgroundColor = "blue";
-}
-
-function addStyleToElement(element, callback) {
-    callback(element);
-}
-
-// Call the function, and pass in div1 (the 
-// DOM element stored in a variable), and pass in the secondd parameter, "makeblue", which is a function. When "MakeBlue" is called, div1 will be passed in. 
-addStyleToElement(div1, makeRed);
-addStyleToElement(div2, makeWhite);
-addStyleToElement(div3, makeBlue);
