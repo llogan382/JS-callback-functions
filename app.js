7e5dc880374a02f3c004ccda7b8981a3686b7e79
Luke Logan <llogan382@gmail.com>
2019-02-28 06:42:24 -0500

    adds background color to dom elements in a callback function

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ function makeRed(element) {
     element.style.backgroundColor = "red";
 }
 
+function makeWhite(element) {
+    element.style.backgroundColor = "white";
+}
+
 function makeBlue(element) {
     element.style.backgroundColor = "blue";
 }
@@ -14,3 +18,9 @@ function makeBlue(element) {
 function addStyleToElement(element, callback) {
     callback(element);
 }
+
+// Call the function, and pass in div1 (the 
+// DOM element stored in a variable), and pass in the secondd parameter, "makeblue", which is a function. When "MakeBlue" is called, div1 will be passed in. 
+addStyleToElement(div1, makeRed);
+addStyleToElement(div2, makeWhite);
+addStyleToElement(div3, makeBlue);
